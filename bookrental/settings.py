25735b2ea4973327465c00ff5b950920d3578e6a
michaelabbeyy134 <michaelabbeyy134@gmail.com>
2026-05-22 12:13:33 +0000

    Update settings.py

--- bookrental/settings.py
+++ bookrental/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get(
 DEBUG = os.environ.get('DEBUG', '1') == '1'
 
 # ✅ CHANGED: allows container, localhost, and your Oracle Linux server IP
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1 0.0.0.0 10.16.209.213').split()
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
